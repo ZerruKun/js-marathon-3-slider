43aad4177491718c653cf4b29f03d4a59b4402c9
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-07-24 12:48:43 +0300

    Bonus. Scroll with keys + wheel and format.

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-const upBtn = document.querySelector('.up-button')
-const downBtn = document.querySelector('.down-button')
+const upBtn = document.querySelector(".up-button");
+const downBtn = document.querySelector(".down-button");
 
-const sidebar = document.querySelector('.sidebar')
+const sidebar = document.querySelector(".sidebar");
 
-const container = document.querySelector('.container')
+const container = document.querySelector(".container");
 
-const mainSlide = document.querySelector('.main-slide')
-const slidesCount = mainSlide.querySelectorAll('div').length
+const mainSlide = document.querySelector(".main-slide");
+const slidesCount = mainSlide.querySelectorAll("div").length;
 
-let activeSlideIndex = 0
+let activeSlideIndex = 0;
 
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
 
-sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+upBtn.addEventListener("click", () => {
+  changeSlide("up");
+});
 
-upBtn.addEventListener('click', () => {
-    changeSlide('up')
-})
+downBtn.addEventListener("click", () => {
+  changeSlide("down");
+});
 
-downBtn.addEventListener('click', () => {
-    changeSlide('down')
-})
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowUp") {
+    changeSlide("up");
+  } else if (event.key === "ArrowDown") {
+    changeSlide("down");
+  }
+});
+
+document.addEventListener("wheel", (event) => {
+  if (event.deltaY > 0) {
+    changeSlide("down");
+  } else if (event.deltaY < 0) {
+    changeSlide("up");
+  }
+});
 
 const changeSlide = (direction) => {
-    if (direction === 'up') {
-        activeSlideIndex++
-        if(activeSlideIndex === slidesCount) {
-            activeSlideIndex = 0
-        }
-    } else if (direction === 'down') {
-        activeSlideIndex--
-        if(activeSlideIndex < 0) {
-            activeSlideIndex = slidesCount - 1
-        }
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex === slidesCount) {
+      activeSlideIndex = 0;
     }
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCount - 1;
+    }
+  }
 
-    const height = container.clientHeight
+  const height = container.clientHeight;
 
-    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
-    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
-}
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+};
